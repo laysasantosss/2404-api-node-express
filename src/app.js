a1fd2947ccd0d77ef91aedae8b5eafde30054430
laysasantosss <106093377+laysasantosss@users.noreply.github.com>
2022-06-13 15:14:28 -0300

    curso node

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import res from "express/lib/response.js";
 import db from "./config/dbConnect.js"
 import routes from "./routes/index.js"
 
@@ -10,5 +11,21 @@ db.once("open", () => {
 const app = express();
 app.use(express.json())
 routes(app);
+
+const livros = [
+  {id: 1, "titulo": "Senhor dos aneis"},
+  {id: 2, "titulo": "O Hobbit"}
+]
+
+
+app.get('/', (req, res) =>{
+  res.status(200).send('Curso de Node, Lay');
+})
+
+app.get('/livros', (req, res) =>{
+  res.status(200).json(livros)
+})
+
+
 
 export default app
